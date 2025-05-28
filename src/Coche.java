bb0be6f709e103203d4d01ee40d8e777cf13ca65
CarlosCA1 <ccostasalonso@danielcastelao.org>
2025-05-28 14:06:14 +0200

    empiezo

--- src/Coche.java
+++ src/Coche.java
@@ -3,10 +3,13 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    int distancia;
+    int litrosGasolina
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0; //determino una velocidad inicial
+        this.distancia = 0;
     }
 }
